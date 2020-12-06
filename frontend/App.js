be579a7d4d8b951b4fe4a9930b3fc679c184d469
rzanabusamra22 <rzanabusamra@gmail.com>
2020-12-06 10:22:48 +0200

    Learn Page

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Signin from './screens/admin-signin'
-import Home from './android/homepage/home-comp'
+// import Signin from './screens/admin-signin'
+// import Home from './android/homepage/home-comp'
+import Learn from './screens/learn'
+//import Card from "./screens/card";
 // import star from './android/imgs/star'
 // <Home />
+const getFonts = () => {
+  return Font.loadAsync({
+      'font1':require('./screens/assests/fonts/Teko-Medium.ttf'),
+      'font2':require('./screens/assests/fonts/Teko-Bold.ttf')
+  })
+}
 export default function App() {
-  return (
-    <Signin />
-  );
+  const [fontsLoaded,setFontsLoaded] =useState(false);
+  if(fontsLoaded){  
+    return (
+    <Learn />
+  )
+}else{  
+  return(
+    <AppLoading
+    startAsync={getFonts}
+    onFinish={()=> setFontsLoaded(true)}
+    />  )}   
 }
   
 const styles = StyleSheet.create({
---
+++ frontend/screens/videos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+export default function Videos(){
+    return(
+        <>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
