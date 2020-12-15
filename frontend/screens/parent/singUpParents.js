4d8bd6fafb4b90561efbadbe53f0539467e9a6ff
rzanabusamra22 <rzanabusamra@gmail.com>
2020-12-15 09:07:55 +0200

    linkedListIntersection

--- frontend/screens/parent/singUpParents.js
+++ frontend/screens/parent/singUpParents.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions,TextInput ,TouchableOpacity} from 'react-native';
 
 
 
@@ -13,9 +13,17 @@ class SignUp extends Component{
 
 render() {
       return (
-          <Text>
+        <View style={styles.container}>
+          <Text style={styles.text}>
               SignUp PARENTS
           </Text>
+          <TextInput placeholder='USER NAME'style={styles.textInput} placeholderTextColor='black'></TextInput>
+          <TextInput placeholder='EMAIL'style={styles.textInput} placeholderTextColor='black'></TextInput>
+          <TextInput placeholder='PASSWORD'style={styles.textInput} placeholderTextColor='black'></TextInput>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
       )
 }
 }
@@ -24,8 +32,33 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      // alignItems: 'center',
+      justifyContent: 'center'
+    },
+    text:{
       alignItems: 'center',
       justifyContent: 'center'
-    }
+    },
+    textInput:{
+      height:50,
+      borderRadius:25,
+      borderWidth:0.5,
+      marginHorizontal:20,
+      paddingLeft:10,
+      marginVertical:5,
+      borderColor:'rgba(0,0,0,0.2)'
+    },
+    button: {
+      backgroundColor: 'white',
+      height: 70,
+      marginHorizontal: 20,
+      borderRadius: 35,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 5,
+      shadowOffset:{width:2,height:2},
+      shadowColor:'black',
+      shadowOpacity:0.2
+   }
   });
 export default SignUp
