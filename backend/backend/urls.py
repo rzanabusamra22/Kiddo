ec62c8f8f6da3229083ee701b00fb596c1d65e46
hamdalla2 <hamdalla2@yahoo.com>
2020-12-16 15:48:51 +0200

    additional tests

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -38,12 +38,12 @@ urlpatterns = [
    path("", include( router.urls )),
    path("", include('djoser.urls')),
    path("", include('djoser.urls.authtoken')), #api/auth/
-   # url(r'^account/', include('djoser.urls')),
+#    url(r'^account/', include('djoser.urls')),
    url(r'^auth/login/', jwt_views.obtain_jwt_token, name='auth'),
    path('checkserver/',views.index,name='index'),
    path('signup/', views.signup, name='signup'),
    path('getid/', views.id, name="id"),
-   path('donate/', views.donate, name="donate")
+#    path('donate/', views.donate, name="donate")
 ]
 
 # /users/id  -> apiGet
