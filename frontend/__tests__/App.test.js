6d75fe734a801506d913faf0397534b022f4d00c
hamdalla2 <hamdalla2@yahoo.com>
2020-12-17 11:16:15 +0200

    mix

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -1,9 +1,8 @@
 import 'react-native';
 import React from 'react'
 import App from '../App';
-// Note: test renderer must be required after react-native.
-import window from '../__mocks__/mocks'; // Must be imported before the tested file
-import { render } from '@testing-library/react-native'
+import '../__mocks__/matchMedia';
+import { render } from '@testing-library/react-native';
 
 it('renders correctly', () => {
   render(<App />);
